d1c18ded49f6f2cda4441327905550c339b2a0aa
Suyash <suyash93@protonmail.com>
2019-09-04 12:05:52 +0530

    add config

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages
+from setuptools import setup
+
+REQUIRED_PACKAGES = ["tensorflow-gpu==2.0.0a0", "tensorflow-datasets==1.0.1"]
+
+setup(
+    name="trainer",
+    version="0.1",
+    install_requires=REQUIRED_PACKAGES,
+    packages=find_packages(),
+    include_package_data=True,
+    description="My Training Package",
+)
